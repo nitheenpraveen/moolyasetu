bdc9b27aaa3efda8d9ae6d5d7b27adc0f5d2bb6b
nitheenpraveen <nitheenpraveenkumar@gmail.com>
2026-02-16 03:00:21 +0530

    Create PrivacyPolicy.js

---
+++ frontend/src/PrivacyPolicy.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function PrivacyPolicy() {
+  return (
+    <div>
+      <h1>Privacy Policy</h1>
+      <p>We collect your email and push notification tokens only to send alerts you request.</p>
+      <p>We do not sell or share your data.</p>
+      <p>You may request deletion of your data at any time.</p>
+    </div>
+  );
+}
