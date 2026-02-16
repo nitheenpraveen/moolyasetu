85f78b1ed4d8c11faf9988c24fc29343299092fb
Nitheen <nitheen@MacBookPro.lan>
2026-02-16 04:52:30 -0500

    Fix: correct get_db import

--- backend/models.py
+++ backend/models.py
@@ -2,6 +2,10 @@ from sqlalchemy import Column, Integer, String, Float, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from .database import SessionLocal
+
+
 import os
 
 Base = declarative_base()
