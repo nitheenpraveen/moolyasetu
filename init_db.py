7482f8c8201f853101df6ec4efba2dc085ec0b4d
nitheenpraveen <nitheenpraveenkumar@gmail.com>
2026-03-01 10:56:24 -0500

    Delete init_db.py

--- init_db.py
+++
@@ -1,6 +0,0 @@
-from database import Base, engine
-from models import PriceAlert, ProductComparison, ContactMessage
-
-print("Creating tables...")
-Base.metadata.create_all(bind=engine)
-print("Done.")
