7a17b0ea885d9fc5066e98bfaf17c18a2bd964c6
nitheenpraveen <nitheenpraveenkumar@gmail.com>
2026-03-01 12:24:22 -0500

    Update next.config.mjs

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,6 +1,21 @@
+// next.config.mjs
+import path from "path";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
+
+  webpack(config) {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@components": path.resolve(__dirname, "frontend/components"),
+      "@app": path.resolve(__dirname, "frontend/app"),
+      "@lib": path.resolve(__dirname, "frontend/lib"),
+      "@utils": path.resolve(__dirname, "frontend/utils")
+    };
+    return config;
+  }
 };
 
 export default nextConfig;
