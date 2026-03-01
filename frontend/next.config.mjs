39ace62b07ea9d5958d2d8ac7dbb5b054237eabc
nitheenpraveen <nitheenpraveenkumar@gmail.com>
2026-03-01 12:46:46 -0500

    Update next.config.mjs

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,14 +1,9 @@
-import path from "path";
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
 
-  // Turbopack config (empty object silences the error if you don't need custom rules)
-  turbopack: {},
-
-  // If you really need aliases, rely on tsconfig.json paths instead.
-  // Turbopack automatically respects tsconfig.json "paths".
+  // Turbopack is default in Next.js 16
+  turbopack: {}
 };
 
 export default nextConfig;
